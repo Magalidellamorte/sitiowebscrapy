2fa34669cc4d090491d08f7634903ffd2c603df0
tomas <tomasrolb@gmail.com>
2025-08-06 21:57:16 -0300

    Change font to Poppins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
 
-// all in fixtures is set to tailwind v3 as interims solutions
-
 const config: Config = {
     darkMode: ["class"],
     content: [
@@ -69,6 +67,9 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif'],
+		},
   		keyframes: {
   			'accordion-down': {
   				from: {
